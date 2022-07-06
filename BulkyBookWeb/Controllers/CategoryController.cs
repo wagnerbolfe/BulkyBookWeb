80a2690627a38eca8317b5e910111657999d4773
Wagner Bolfe <shaper@live.com>
2022-07-06 13:36:24 -0300

    CRUD MVC

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -37,6 +37,7 @@ namespace BulkyBookWeb.Controllers
 
             _db.Categories.Add(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
             return RedirectToAction("Index");
         }
 
@@ -70,7 +71,7 @@ namespace BulkyBookWeb.Controllers
 
             _db.Categories.Update(obj);
             _db.SaveChanges();
-
+            TempData["success"] = "Category updated successfully";
             return RedirectToAction("Index");
         }
 
@@ -106,7 +107,7 @@ namespace BulkyBookWeb.Controllers
 
             _db.Categories.Remove(obj);
             _db.SaveChanges();
-
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
     }
